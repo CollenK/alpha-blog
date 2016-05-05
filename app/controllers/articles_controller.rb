e756f9bf4999d943d18972aa3c0e2f5ac398d0c6
Udemy Rails Course <collen.kriel@gmail.com>
2016-05-05 13:04:11 +0000

    add article meta data and style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
         redirect_to article_path(@article)   
         else
             render 'new' 
-        end
+       end
     end
 
     def update
